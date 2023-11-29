2fa8515dae8fd83744a88d63b9189791819bc3dd
vyportfolio1 <vyportfolio1@gmail.com>
2023-11-29 15:24:16 +0200

    burger meniu

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const burgerIcon = document.querySelector(".fa-bars");
+const xIcon = document.querySelector(".fa-x");
+const slideMeniu = document.querySelector(".slide-meniu");
+
+burgerIcon.addEventListener("click", () => {
+    slideMeniu.classList.remove("invisible");
  
+});
+
+xIcon.addEventListener("click", () => {
+    slideMeniu.classList.add("invisible");
+ 
+});
